5f41972ec586bdb725f275e2a8ba36d893913850
Benedikt Schindler <benedikt.schindler@owayo.com>
2020-12-15 15:53:15 +0100

    SCP bzw. SFTP (SCP 2.0) hinzugefügt.

--- uDevartSSHConnector.pas
+++ uDevartSSHConnector.pas
@@ -2,20 +2,24 @@ unit uDevartSSHConnector;
 
 interface
 
-uses uISSHConnector, ScSSHClient, ScSSHUtils, ScSSHChannel, ScBridge, Types,
-     SysUtils;
+uses uISSHConnector, ScSSHClient, ScSFTPClient, ScSSHUtils, ScSSHChannel, ScBridge, Types,
+     SysUtils, Classes, ScSFTPUtils;
 
   type TDevartSSHConnector = class(TInterfacedObject, ISSHConnector)
   private
     ScSSHShell: TScSSHShell;
     ScSSHClient: TScSSHClient;
+    SFTPClient : TScSFTPClient;
     ScMemoryStorage: TScMemoryStorage;
     procedure ScSSHClient1ServerKeyValidate(Sender: TObject; NewServerKey: TScKey; var Accept: Boolean);
     procedure OnAuth(Sender: TObject; const Name, Instruction: string; const Prompts: TStringDynArray; var Responses: TStringDynArray);
+    procedure DoOnVersionSelect(Sender: TObject;
+      const Versions: TScSFTPVersions; var Version: TScSFTPVersion);
   public
     function ExecuteCommand(aCommand : string) : string;
     constructor Create(const host: string; const port: integer;  const username, password : string; UseKeyBoardInteractive : boolean = false); reintroduce;
     destructor Destroy; override;
+    procedure DownloadFileToStream(const SourceName:string; destination : TStream; FileOffset : Int64 = 0);
   end;
 
 implementation
@@ -51,14 +55,40 @@ begin
   ScSSHShell := TScSSHShell.Create(nil);
   ScSSHShell.UseUnicode := True;
   ScSSHShell.Client := ScSSHClient;
+
+  SFTPClient := TScSFTPClient.create(nil);
+  SFTPClient.UseUnicode := True;
+  SFTPClient.SSHClient := ScSSHClient;
+  SFTPClient.OnVersionSelect := DoOnVersionSelect;
+end;
+
+procedure TDevartSSHConnector.DoOnVersionSelect(Sender: TObject; const Versions: TScSFTPVersions; var Version: TScSFTPVersion);
+begin
+  for var v in Versions
+    do if v > Version then Version := v;
 end;
 
 destructor TDevartSSHConnector.Destroy;
 begin
+  try
+    if SFTPClient.Active
+      then SFTPClient.Disconnect;
+  except
+  end;
+
+  SFTPClient.free;
   ScSSHShell.Free;
   ScSSHClient.Free;
   ScMemoryStorage.Free;
   inherited;
+end;
+
+procedure TDevartSSHConnector.DownloadFileToStream(const SourceName: string;  destination: TStream; FileOffset: Int64 = 0);
+begin
+  if not SFTPClient.Active
+    then SFTPClient.Initialize;
+
+  SFTPClient.DownloadToStream(SourceName, destination, FileOffset);
 end;
 
 function TDevartSSHConnector.ExecuteCommand(aCommand: string): string;
--- uISSHConnector.pas
+++ uISSHConnector.pas
@@ -1,10 +1,12 @@
 unit uISSHConnector;
 
-
 interface
+
+uses Classes;
 
 type ISSHConnector = Interface
   function ExecuteCommand(aCommand : string) : string;
+  procedure DownloadFileToStream(const SourceName:string; destination : TStream; FileOffset : Int64 = 0);
 end;
 
 
